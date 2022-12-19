341760252fa0abcd4d658dc21848c22a9776d5d1
Fred Duarte <fduartej@gmail.com>
2022-12-18 23:24:20 -0500

    Agredando la app

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import geopandas as gpd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 
+def cargarDepartamentos():
+    values = departamentos['NOMBDEP'].tolist()
+    st.selectbox('Selecciona el departamento:',values,on_change=mostrarMapa)
+    
+
+def mostrarMapa():
+    departamento = "ANCASH" #seleccionar item
+    centro_vacunacion_filter = centro_vacunacion_all[(centro_vacunacion_all.departamento == departamento)]
+    seleccion_geometria = gpd.GeoDataFrame(centro_vacunacion_filter, geometry=gpd.points_from_xy(centro_vacunacion_filter.longitud, centro_vacunacion_filter.latitud))
+    selecion_geometria01=seleccion_geometria[(seleccion_geometria['latitud']!=0)|(seleccion_geometria['longitud']!=0)]
+    eleccion=departamentos[departamentos['NOMBDEP']==departamento]
+    fig, ax = plt.subplots(figsize = (8,10))
+    eleccion.plot(ax = ax, color = 'White', edgecolor = 'grey')
+    selecion_geometria01.plot(ax = ax, color = 'Red')
+
+#main
+departamentos = gpd.read_file('peru_departamental_simple.geojson')
 centro_vacunacion = pd.read_csv('TB_CENTRO_VACUNACION.csv',delimiter=",")
-centro_vacunacion.head()
-
 ubigeo = pd.read_csv('TB_UBIGEOS.csv',delimiter=",")
-ubigeo.head()
+ubigeo_subset = ubigeo[["id_ubigeo", "departamento"]]
+centro_vacunacion_all=pd.merge(centro_vacunacion,ubigeo_subset,on='id_ubigeo')
 
-option = st.selectbox('Selecciona el departamento:',('AMAZONAS','ANCASH','APURIMAC','AREQUIPA','AYACUCHO','CAJAMARCA','CALLAO','CUSCO','HUANCAVELICA','HUANUCO','ICA','JUNIN','LA LIBERTAD','LAMBAYEQUE','LIMA','LORETO','MADRE DE DIOS','MOQUEGUA','PASCO','PIURA','PUNO','SAN MARTIN','TACNA','TUMBES','UCAYALI'))
-st.write('Seleccionó:', option)
+cargarDepartamentos()
+
+
+    
+    
